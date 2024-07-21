cacfac41a41cfd74cd6cd79b871082b1b7c48bd3
LuciusCat <cesar.antonio.g_555@hotmail.com>
2024-07-20 22:10:33 -0500

    fix: arreglando problema con la asincronia

--- public/img/gruas/index.js
+++ public/img/gruas/index.js
@@ -1,16 +1,49 @@
-const imageModules = import.meta.glob('./*.webp');
+import grua1 from './grua-1.webp';
+import grua2 from './grua-2.webp';
+import grua3 from './grua-3.webp';
+import grua4 from './grua-4.webp';
+import grua5 from './grua-5.webp';
+import grua6 from './grua-6.webp';
+import grua7 from './grua-7.webp';
+import grua8 from './grua-8.webp';
+import grua9 from './grua-9.webp';
+import grua10 from './grua-10.webp';
+import grua11 from './grua-11.webp';
+import grua12 from './grua-12.webp';
+import grua13 from './grua-13.webp';
+import grua14 from './grua-14.webp';
+import grua15 from './grua-15.webp';
+import grua16 from './grua-16.webp';
+import grua17 from './grua-17.webp';
+import grua18 from './grua-18.webp';
+import grua19 from './grua-19.webp';
+import grua20 from './grua-20.webp';
+import grua21 from './grua-21.webp';
+import grua22 from './grua-22.webp';
 
-const imagePromises = [];
-
-for (const path in imageModules) {
-    imagePromises.push(imageModules[path]().then((module) => ({
-        original: module.default,
-        thumbnail: module.default,
-    })));
-}
-
-const images = await Promise.all(imagePromises);
-
-export { images };
+export const images = [
+    {original: grua1, thumbnail: grua1},
+    {original: grua2, thumbnail: grua2},
+    {original: grua3, thumbnail: grua3},
+    {original: grua4, thumbnail: grua4},
+    {original: grua5, thumbnail: grua5},
+    {original: grua6, thumbnail: grua6},
+    {original: grua7, thumbnail: grua7},
+    {original: grua8, thumbnail: grua8},
+    {original: grua9, thumbnail: grua9},
+    {original: grua10, thumbnail: grua10},
+    {original: grua11, thumbnail: grua11},
+    {original: grua12, thumbnail: grua12},
+    {original: grua13, thumbnail: grua13},
+    {original: grua14, thumbnail: grua14},
+    {original: grua15, thumbnail: grua15},
+    {original: grua16, thumbnail: grua16},
+    {original: grua17, thumbnail: grua17},
+    {original: grua18, thumbnail: grua18},
+    {original: grua19, thumbnail: grua19},
+    {original: grua20, thumbnail: grua20},
+    {original: grua21, thumbnail: grua21},
+    {original: grua22, thumbnail: grua22},
+]
 
 
