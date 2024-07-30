088795a11415e57050ea91b04005d910eebcf367
LuciusCat <cesar.antonio.g_555@hotmail.com>
2024-07-29 19:12:07 -0500

    feat (form): manejo de errores form

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -1,11 +1,58 @@
+import { useState, useEffect } from "react";
 import styles from "./styles/form.module.css";
 
 export default function Form() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    // Verificar si el mensaje ha sido enviado en la recarga de la página
+    if (sessionStorage.getItem("formSubmitted") === "true") {
+      alert("El mensaje fue enviado con éxito.");
+      sessionStorage.removeItem("formSubmitted"); // Limpiar el estado
+    }
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevenir el envío por defecto
+
+    setIsSubmitting(true); // Cambiar el estado del botón
+
+    // Crear un FormData para enviar los datos del formulario
+    const formData = new FormData(event.target);
+
+    // Enviar los datos del formulario usando fetch
+    fetch(event.target.action, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Guardar el estado en sessionStorage
+          sessionStorage.setItem("formSubmitted", "true");
+          // Redirigir a la URL una vez que el formulario haya sido enviado correctamente
+          window.location.href = "https://luciuscat.github.io/gruas-edeca/";
+        } else {
+          // Manejar el error si el formulario no se envía correctamente
+          setIsSubmitting(false);
+          alert("Ocurrió un error al enviar el formulario.");
+        }
+      })
+      .catch(() => {
+        // Manejar cualquier error en el envío
+        setIsSubmitting(false);
+        alert("Ocurrió un error al enviar el formulario.");
+      });
+  };
+
   return (
     <form
       action="https://formsubmit.co/cesar.antonio.g_555@hotmail.com"
       method="POST"
       className={styles.form}
+      onSubmit={handleSubmit}
     >
       <fieldset>
         <legend>Déjenos un mensaje</legend>
@@ -34,12 +81,9 @@ export default function Form() {
           required
           placeholder="Ingrese acá su mensaje"
         ></textarea>
-        <button type="submit">Enviar</button>
-        <input
-          type="hidden"
-          name="_next"
-          value="http://localhost:5173/gruas-edeca"
-        />
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Enviando mensaje..." : "Enviar"}
+        </button>
         <input type="hidden" name="_captcha" value="false" />
       </fieldset>
     </form>
